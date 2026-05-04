11236eb61fd3f80586ae615d297da4772821b7cd
Полина Медведева <hello.polina-LA@yandex.ru>
2026-05-04 12:13:16 +0300

    Fix comments for dz4

--- src/dz4.java
+++ src/dz4.java
@@ -17,61 +17,49 @@ public class dz4 {
             }
         }
 
-        StringBuilder message = new StringBuilder();
-        for (String line : arr) {
-            message.append(line).append("#");
-        }
-
-        System.out.printf("Расшифрованное послание: %s", message);
+        System.out.printf("Расшифрованное послание: %s", String.join("#", arr));
 
 //    2 Условия&Циклы: «Ночная смена в QA-департаменте»
-
-        String[] autotests = new String[100 + 1];
 
         int countPass = 0;
         int countCritical = 0;
         int countBug = 0;
         int countFlaky = 0;
 
-        boolean showOnlyIssues = true;
+        boolean showOnlyIssues = false;
         boolean stopSignal = false;
 
-        for (int i = 0; i < autotests.length; i++) {
-            if (i == 0) {
-                autotests[i] = "Health check";
-            } else if (i % 3 == 0 && i % 5 == 0) {
-                if(countCritical >= 3) {
+        for (int i = 1; i < 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                if(countCritical >= 2) {
                     System.out.println("\uD83D\uDEA8 Слишком много критических багов — будим тимлида!");
                     stopSignal = true;
                     break;
                 }
                 else {
-                    autotests[i] = "Critical!";
-                    System.out.printf("Тест #%d: %s", i, autotests[i] + "\n");
+                    System.out.printf("Тест #%d: %s", i, "Critical!" + "\n");
                     countCritical++;
                 }
             } else if (i % 5 == 0) {
-                autotests[i] = "Bug";
-                System.out.printf("Тест #%d: %s", i, autotests[i] + "\n");
+                System.out.printf("Тест #%d: %s", i, "Bug" + "\n");
                 countBug++;
             } else if (i % 3 == 0) {
-                autotests[i] = "Flaky";
-                System.out.printf("Тест #%d: %s", i, autotests[i] + "\n");
+                System.out.printf("Тест #%d: %s", i, "Flaky" + "\n");
                 countFlaky++;
             } else {
-                autotests[i] = "Pass";
                 countPass++;
-                if(showOnlyIssues && autotests[i].equals("Pass")) {
-                    continue;
+                if(showOnlyIssues) {
+                    System.out.printf("Тест #%d: %s", i, "Pass" + "\n");
                 }
-                else System.out.printf("Тест #%d: %s", i, autotests[i] + "\n");
             }
         }
+
+        int totalTest = countPass + countCritical + countBug + countFlaky;
 
         if(!stopSignal) {
             System.out.println("\n" +
                     "===== ИТОГИ НОЧНОЙ СМЕНЫ =====" + "\n" +
-                    "Всего тестов: " + (autotests.length - 1) + "\n" +
+                    "Всего тестов: " + totalTest + "\n" +
                     "Pass: " + countPass + "\n" +
                     "Flaky: " + countFlaky + "\n" +
                     "Bug: " + countBug + "\n" +
